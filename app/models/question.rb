9904a3c4aa89fb3a8b39560666db3f0009fa5c8d
shoichinishiguchi <shoichi.nishiguchi@gi-no.jp>
2019-10-09 08:37:37 +0900

    delete_time_to_ans_to_i

--- app/models/question.rb
+++ app/models/question.rb
@@ -47,24 +47,4 @@ class Question < ApplicationRecord
     day = sec/3600/24
     "次の解答まで、あと#{day.to_s}日#{hour.to_s}時間#{min.to_s}分"
   end
-
-  def time_to_ans_to_i
-    remaining_time = (self.last_ans_date - Time.now).to_i
-    case importance
-    when 0 # +6ヶ月
-      remaining_time += 6*30*24*60*60
-    when 1 # +3ヶ月
-      remaining_time += 3*30*24*60*60
-    when 2 # +1ヶ月
-      remaining_time += 1*30*24*60*60
-    when 3 # +7日
-      remaining_time += 7*24*60*60
-    when 4 # +3日
-      remaining_time += 3*24*60*60
-    when 5 # 12時間後
-      remaining_time += 12*60*60
-    end
-    sec = remaining_time + 60 #表示上60秒微調整
-  end
-
 end
